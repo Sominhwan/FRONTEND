07915d9a45bc5aa31096299ad64e3fd699be3a46
Sominhwan <thalsghks@naver.com>
2023-08-22 23:59:03 +0900

    20230822 SMH 회원가입 양식 추가

--- src/views/signUp/SignUpView.vue
+++ src/views/signUp/SignUpView.vue
@@ -12,32 +12,102 @@
                     <v-col cols="12" md="8" offset="2">
                         <v-card-text>
                             <v-text-field
+                                ref="email"
+                                v-model="email"
                                 dense
-                                label="Outlined"
+                                label="아이디(이메일)"
+                                solo
+                                flat
                                 outlined
+                                prepend-inner-icon="person"
+                                suffix="@naver.com"
+                                :rules="[() => !!email || '* 아이디(이메일): 필수정보입니다.']"
                                 required
                             ></v-text-field>
                             <v-text-field
+                                ref="password"
+                                v-model="password"
                                 dense
-                                label="Outlined"
+                                label="비밀번호"
+                                flat
+                                password
                                 outlined
+                                prepend-inner-icon="lock"
                                 counter="25"
+                                :rules="[() => !!password || '* 비밀번호: 필수정보입니다.']"
                                 required
                             ></v-text-field>
                             <v-text-field
+                                ref="name"
+                                v-model="name"
                                 dense
-                                label="Outlined"
+                                label="이름"
+                                flat
                                 outlined
+                                prepend-inner-icon="person"
+                                :rules="[() => !!name || '* 이름: 필수정보입니다.']"
                                 required
                             ></v-text-field>
                             <v-text-field
+                                ref="name"
+                                v-model="birthdate"
                                 dense
-                                label="Outlined"
+                                label="생년월일"
+                                flat
                                 outlined
+                                prepend-inner-icon="calendar_month"
+                                :rules="[() => !!name || '* 생년월일: 필수정보입니다.']"
                                 required
                             ></v-text-field>
+                            <v-row>
+                                <v-col>
+                                    <v-select
+                                        :items="gender"
+                                        dense
+                                        label="성별"
+                                        outlined
+                                        flat
+                                        solo
+                                    ></v-select>
+                                </v-col>
+                                <v-col>
+                                    <v-select
+                                        :items="division"
+                                        v-model="select"
+                                        dense
+                                        outlined
+                                        flat
+                                        solo
+                                    ></v-select>
+                                </v-col>
+                            </v-row>
+                                <div style="display: flex;;">
+                                <v-text-field
+                                    style="width: 300px; margin-right: 20px;"
+                                    ref="phonenum"
+                                    v-model="phonenum"
+                                    dense
+                                    label="휴대폰번호"
+                                    flat
+                                    outlined
+                                    prepend-inner-icon="phone_iphone"
+                                    :rules="[() => !!name || '* 휴대폰번호: 필수정보입니다.']"
+                                    required
+                                ></v-text-field>
+                                <v-select
+                                    style="width: 100px;"
+                                    :items="telecom"
+                                    label="통신사"
+                                    dense
+                                    outlined
+                                    flat
+                                    solo
+                                    prepend-inner-icon="cell_tower"
+                                ></v-select>
+                                </div>
                         </v-card-text>
                     </v-col>
+                    <!-- TODO Vuetify Treeview 네이버 약관동의 참고하여 추가하기 --> 
                     <v-text-field
                         class="sign-loading"
                         color="primary"
@@ -52,7 +122,17 @@
 
 <script>
 export default {
-    
+    data: () => ({
+        name: null,
+        email: null,
+        password: null,
+        birthdate: null,
+        phonenum: null,
+        gender: ['남자', '여자'],
+        division: ['내국인', '외국인'],
+        select: '내국인',
+        telecom: ['SKT', 'KT', 'LG U+']
+    }),
 }
 </script>
 
