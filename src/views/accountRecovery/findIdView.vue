373886fa0cb5cbbeb58aff731d795d1772af906d
Sominhwan <thalsghks@naver.com>
2023-10-04 23:50:39 +0900

    20231004 SMH 아이디 찾기 폼 개발중

--- src/views/accountRecovery/findIdView.vue
+++ src/views/accountRecovery/findIdView.vue
@@ -1,10 +1,25 @@
 <template>
-    <div></div>
+    <v-card-title>
+        <v-breadcrumbs :items="accountTabTitle" large>
+            <template v-slot:item="{ item }">
+                <v-breadcrumbs-item :disabled="item.disabled">
+                    <span class="account-title" :style="`color: ${item.color}`" @click="$router.push({name: 'findPwd'})"> {{ item.text }}</span>
+                </v-breadcrumbs-item>
+            </template>
+        </v-breadcrumbs>
+        <div class="additional-text">아이디를 찾이 위한 본인확인 방법을 선택하세요.</div>
+    </v-card-title>
 </template>
 <script>
 export default {
     data () {
       return { 
+        accountTab: null,
+        accountTabTitle: [
+            { text: '아이디 찾기', disabled: true, href: '/findid', color: '#2889f1'}, 
+            { text: '비밀번호 찾기', disabled: false, href: '/findpwd', color: 'grey'}
+        ],
+        text: 'Lor',
       }
     },   
     mounted() {
@@ -13,5 +28,13 @@ export default {
 }
 </script>
 <style scoped>
-
+ .account-title{
+    cursor: pointer;
+ }
+ .additional-text {
+    display: inline-block; /* 텍스트를 가리지 않도록 인라인 블록으로 설정 */
+    margin-left: 20px; /* 원하는 여백 설정 */
+    font-size: 18px;
+    font-weight: bold;
+ }
 </style>
